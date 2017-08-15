00e5c87571e1dce2566a117dbe272d598a124252
Ḟenix෴ᙞinario <fbamperius@gmail.com>
2017-08-15 20:33:42 +0200

    Update extra.cpp

--- NAVE/extra.cpp
+++ NAVE/extra.cpp
@@ -1,14 +1,14 @@
 // reemplazar esto:
 
 void exec(std::string args, bool block) {
-    pid_t pid = fork();  // This process executes a given program
+    pid_t pid = fork(); 
 
     if (pid == -1) {
         error("fatal fork error", true);
         exit(1);
-    } else if (pid == 0) { // This is the child process
+    } else if (pid == 0) { 
         child(args);
-    } else { // This is the parent process.
+    } else { 
         parent(pid, block);
     }
 }
@@ -17,6 +17,6 @@ void exec(std::string args, bool block) {
 //con este:
 
 void exec(std::string args, bool block) {
-    pid_t pid = fork();  // This process executes a given program
+    pid_t pid = fork();  
     ((pid == -1) ? ((pid == 0) ? child(args) : parent(pid, block)) : error("fatal fork error", true));
 }
